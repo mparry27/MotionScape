60023a78c8bfb82708c2d44791f616f058a26cba
Mason Parry <mason111parry@gmail.com>
2020-09-29 17:50:28 -0600

    Smoothed velocity more and seperated logic from GUI

---
+++ KinectMathGames/Domain/Kinect.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Kinect;
+
+namespace KinectMathGames.Domain
+{
+    class Kinect : INotifyPropertyChanged
+    {
+        private KinectSensor sensor;
+        private long prevTimeStamp = 0;
+        private float zpos = 0;
+        private float prevZPos = 0;
+        private float vel = 0;
+        private float prevVel = 0;
+        private float velGate = 0.02F;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public float zPosition
+        { 
+            get
+            {
+                return zpos;
+            } 
+            set 
+            {
+                zpos = value;
+                NotifyPropertyChanged("zPosition"); 
+            }
+        }
+        public float velocity
+        {
+            get
+            {
+                return vel;
+            }
+            set
+            {
+                vel = value;
+                NotifyPropertyChanged("velocity");
+            }
+        }
+
+        public Kinect()
+        {
+            if (KinectSensor.KinectSensors.Count > 0)
+            {
+                sensor = KinectSensor.KinectSensors[0];
+                sensor.AllFramesReady += this.SensorAllFramesReady;
+            }
+            sensor.Start();
+            sensor.SkeletonStream.Enable();
+        }
+
+
+        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+        Skeleton[] skeletons = new Skeleton[0];
+        long timeStamp = 0;
+
+        using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+        {
+            if (skeletonFrame != null)
+            {
+                skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                skeletonFrame.CopySkeletonDataTo(skeletons);
+                timeStamp = skeletonFrame.Timestamp;
+            }
+        }
+
+        if (skeletons.Length != 0)
+        {
+            foreach (Skeleton skel in skeletons)
+            {
+                if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                {
+                    zpos = skel.Joints[JointType.Spine].Position.Z;
+                    vel = (zpos - prevZPos) * (1000 / (timeStamp - prevTimeStamp)) * -1;
+                    if ((vel - prevVel) > velGate)
+                        vel = prevVel + velGate;
+                    if ((vel - prevVel) < (-1 * velGate))
+                        vel = prevVel + (-1 * velGate);
+
+                    prevZPos = zpos;
+                    zPosition = zpos;
+                    prevVel = vel;
+                    velocity = vel;
+                    prevTimeStamp = timeStamp;
+                }
+            }
+        }
+    }
+}
+
+    
+}
--- KinectMathGames/Gui/MainWindow.xaml.cs
+++ KinectMathGames/Gui/MainWindow.xaml.cs
@@ -14,7 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Kinect;
+using KinectMathGames.Domain;
+
 
 namespace KinectMathGames
 {
@@ -23,66 +24,11 @@ namespace KinectMathGames
     /// </summary>
     public partial class MainWindow : Window
     {
-        private KinectSensor sensor;
-        private long prevTimeStamp = 0;
-        private float prevZPos = 0;
-        private float avgSum = 0;
-        private int avgCount = 0;
-        private int avgAcuracy = 5;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if (KinectSensor.KinectSensors.Count > 0)
-            {
-                sensor = KinectSensor.KinectSensors[0];
-                sensor.AllFramesReady += this.SensorAllFramesReady;
-            }
-            sensor.Start();
-            sensor.SkeletonStream.Enable();
-        }
-
-        private void SensorAllFramesReady(object sender, AllFramesReadyEventArgs e)
-        {
-            Skeleton[] skeletons = new Skeleton[0];
-            long timeStamp = 0;
-
-            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
-            {
-                if (skeletonFrame != null)
-                {
-                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
-                    skeletonFrame.CopySkeletonDataTo(skeletons);
-                    timeStamp = skeletonFrame.Timestamp;
-                }
-            }
-
-            if (skeletons.Length != 0)
-            {
-                foreach(Skeleton skel in skeletons) {
-                    if (skel.TrackingState == SkeletonTrackingState.Tracked)
-                    {
-                        var zpos = skel.Joints[JointType.Spine].Position.Z;
-                        var vel = (zpos - prevZPos) * (1000 /(timeStamp - prevTimeStamp))*-1;
-                        avgSum += vel;
-                        avgCount++;
-                        if(avgCount >= avgAcuracy)
-                        {
-                            lblDepth.Content = zpos.ToString("0.00") + " m";
-                            lblVelocity.Content = vel.ToString("0.00") + " m/s";
-                            sldrVelocity.Value = avgSum/avgCount;
-                            sldrPosition.Value = zpos;
-                            avgSum = 0;
-                            avgCount = 0;
-                        }
-                        prevZPos = zpos;
-                        prevTimeStamp = timeStamp;
-                    }
-                }
-            }
-        }
+            Kinect kinect = new Kinect();
+            this.DataContext = kinect;
+        }     
     }
 }
