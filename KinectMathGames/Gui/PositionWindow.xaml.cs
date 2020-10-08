2a8a9f3b368a732fd67d2388de785dccb1950670
Ben Bos <bennyb1097@yahoo.com>
2020-10-08 11:14:17 -0600

    Added Position game window and Images folder
    I copied the code for MainWindow and modified it to look like what we talked about for the position window.  I hope it doesn't screw anyting up since I just copied it.  I added an images folder that for now just holds the grid image as a png.

---
+++ KinectMathGames/Gui/PositionWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KinectMathGames.Domain;
+
+
+namespace KinectMathGames
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class PositionWindow : Window
+    {
+        public PositionWindow()
+        {
+            InitializeComponent();
+            Kinect kinect = new Kinect();
+            this.DataContext = kinect;
+        }     
+    }
+}
