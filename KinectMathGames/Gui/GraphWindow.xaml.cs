18d81ce1cd16dcbba3d16e9d9dad1aa11b9c914b
Mason Parry <mason111parry@gmail.com>
2020-11-12 15:01:25 -0700

    Added graph guessing game functionallity

--- KinectMathGames/Gui/GraphWindow.xaml.cs
+++ KinectMathGames/Gui/GraphWindow.xaml.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
 using KinectMathGames.Domain;
@@ -24,9 +17,14 @@ namespace KinectMathGames.Gui
         double scale = 50;
         Kinect sensor = new Kinect();
         Line slope = new Line() { Stroke = Brushes.Green, StrokeThickness = 2, SnapsToDevicePixels = true };
-        Line playerHitLine1 = new Line() { Stroke = Brushes.Green, StrokeThickness = 2, SnapsToDevicePixels = true };
-        Line playerHitLine2 = new Line() { Stroke = Brushes.Green, StrokeThickness = 2, SnapsToDevicePixels = true };
+        Line playerHitLine1 = new Line() { SnapsToDevicePixels = true };
+        Line playerHitLine2 = new Line() { SnapsToDevicePixels = true };
+        int round = 0;
+        int seconds = 0;
+        int score = 0;
         Random random = new Random();
+        DispatcherTimer roundTimer = new DispatcherTimer();
+        DispatcherTimer tickTimer = new DispatcherTimer();
         public GraphWindow()
         {
             InitializeComponent();
@@ -40,23 +38,19 @@ namespace KinectMathGames.Gui
                 MyCanvas.Children.Add(line2);
             }
 
+            // Add slope and hit detection lines
             MyCanvas.Children.Add(slope);
             MyCanvas.Children.Add(playerHitLine1);
             MyCanvas.Children.Add(playerHitLine2);
-            Generate_Point();
             
+            // Start ticker for every frame generated.
+            tickTimer.Tick += Timer_Tick;
+            tickTimer.Interval = TimeSpan.FromMilliseconds(20);
+            tickTimer.Start();
 
-            DispatcherTimer dispatcherTimer = new DispatcherTimer();
-            dispatcherTimer.Tick += Timer_Tick;
-
-            dispatcherTimer.Interval = TimeSpan.FromMilliseconds(10);
-            dispatcherTimer.Start();
-
-            DispatcherTimer dispatcherTimer1 = new DispatcherTimer();
-            dispatcherTimer1.Tick += Big_Tick;
-
-            dispatcherTimer1.Interval = TimeSpan.FromSeconds(20);
-            dispatcherTimer1.Start();
+            // Set up 1 second tick event
+            roundTimer.Tick += RoundTimer;
+            roundTimer.Interval = TimeSpan.FromSeconds(1);
         }
 
         private void Timer_Tick(object sender, EventArgs e)
@@ -75,19 +69,6 @@ namespace KinectMathGames.Gui
             playerHitLine2.Y1 = Canvas.GetTop(rec1) - 1;
             playerHitLine2.X2 = playerHitLine2.X1;
             playerHitLine2.Y2 = playerHitLine2.Y1 + 12;
-
-            if(IsIntersecting(new Point(playerHitLine1.X1, playerHitLine1.Y1), new Point(playerHitLine1.X2, playerHitLine1.Y2), new Point(slope.X1, slope.Y1), new Point(slope.X2, slope.Y2)))
-            {
-                textBlock.Text = "Detected";
-            }
-            else if (IsIntersecting(new Point(playerHitLine2.X1, playerHitLine2.Y1), new Point(playerHitLine2.X2, playerHitLine2.Y2), new Point(slope.X1, slope.Y1), new Point(slope.X2, slope.Y2)))
-            {
-                textBlock.Text = "Detected";
-            }
-            else
-            {
-                textBlock.Text = "Not Detected";
-            }
         }
 
         private bool IsIntersecting(Point a, Point b, Point c, Point d)
@@ -105,13 +86,80 @@ namespace KinectMathGames.Gui
             return (r >= 0 && r <= 1) && (s >= 0 && s <= 1);
         }
 
-        private void Big_Tick(object sender, EventArgs e)
+        private void StartGame()
         {
-            Generate_Point();
+            score = 0;
+            round = 0;
+            seconds = 0;
+            startResetButton.Content = "Reset";
+
+            roundTimer.Start();
+
+            GenerateRandomLine();
+            textBlock.FontSize = 8;
+            textBlock.Text = "\nTime: " + 10 + "\n\nScore: " + score;
         }
 
-        private void Generate_Point()
+        private void RoundTimer(object sender, EventArgs e)
         {
+            seconds += 1;
+            int secondsDisplay;
+            if(round < 5)
+            {
+                if (seconds == 10)
+                {
+                    slope.Visibility = Visibility.Visible;
+                    if (IsIntersecting(new Point(playerHitLine1.X1, playerHitLine1.Y1), new Point(playerHitLine1.X2, playerHitLine1.Y2), new Point(slope.X1, slope.Y1), new Point(slope.X2, slope.Y2))
+                        || IsIntersecting(new Point(playerHitLine2.X1, playerHitLine2.Y1), new Point(playerHitLine2.X2, playerHitLine2.Y2), new Point(slope.X1, slope.Y1), new Point(slope.X2, slope.Y2)))
+                    {
+                        
+                        score++;
+                    }
+                }
+                else if (seconds == 14)
+                {
+                    round++;
+                    GenerateRandomLine();
+                    seconds = 0;
+                }
+                if (seconds > 10)
+                    secondsDisplay = 0;
+                else
+                    secondsDisplay = 10 - seconds;
+                textBlock.Text = "\nTime: " + secondsDisplay + "\n\nScore: " + score;
+            }
+            else
+            {
+                roundTimer.Stop();
+                startResetButton.Content = "Play Again?";
+                textBlock.Text = "\nTotal Score: " + score;
+            }
+        }
+
+        private void ResetGame()
+        {
+            roundTimer.Stop();
+            pointSlopeLbl.Text = "Graph Guesser";
+
+            textBlock.Text = "";
+            Run run = new Run
+            {
+                FontWeight = FontWeights.Bold,
+                FontSize = 3.5,
+                Text = "                             THE GOAL"
+            };
+            textBlock.Inlines.Add(run);
+            run.FontWeight = FontWeights.Normal;
+            textBlock.Inlines.Add(new LineBreak());
+            run.Text = ("■    Score as many points by correctly guessing where the  ");
+            textBlock.Inlines.Add(run);
+
+            startResetButton.Content = "Start";
+        }
+
+        private void GenerateRandomLine()
+        {
+            slope.Visibility = Visibility.Hidden;
             double pointX = 50+random.Next(-3, 3) * 10;
             double pointY = 50+random.Next(-3, 3) * 10;
             double slopeRise = random.Next(-5, 5) * 10;
@@ -144,9 +192,24 @@ namespace KinectMathGames.Gui
             pointSlopeLbl.Text += Math.Abs((pointX / 10) - 5) + ")";
         }
 
-        private void Remove_Point(object sender, EventArgs e)
+        private void QuitClick(object sender, RoutedEventArgs e)
         {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
+        }
 
+        private void StartResetClick(object sender, RoutedEventArgs e)
+        {
+            String state = (sender as Button).Content.ToString();
+            if(state == "Start")
+            {
+                StartGame();
+            } 
+            else
+            {
+                ResetGame();
+            }
         }
     }
 }
